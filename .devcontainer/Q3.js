7e762a0197f261f9f4bdcce1fd2feef156bfbc53
Anush MK <101824229+Anushmk29@users.noreply.github.com>
2024-02-10 17:36:35 +0000

    done

--- .devcontainer/Q3.js
+++ .devcontainer/Q3.js
@@ -1,8 +1,3 @@
-// Merge Arrays without Duplicates with functions
-function mergeArr(arr,arr2){
-   let sumarr=arr+arr2
-   console.log(sumarr)
-}
 // Sum of Array Elements
 let arr =  [1, 2, 3, 4]
 let arr2 = [3, 4, 5]
@@ -12,4 +7,3 @@ for(let i =0;i<=arr.length;i++)
    sum+=i;
 }
 console.log(sum)
-mergeArr(arr,arr2)
