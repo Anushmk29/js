cd3be6db45a99037481a9abe17074a21bba5f2cd
Anush MK <101824229+Anushmk29@users.noreply.github.com>
2024-02-19 21:03:46 +0000

    done

---
+++ .devcontainer/objects/objdestructure.js
@@ -0,0 +1,24 @@
+// objects destructuring can be done for array also destrcuturing
+
+// const user = {
+//     name:"anush",
+//     salary:"10k",
+//     company_name:"infoysys"
+// }
+
+//this method , to print multiple times we have to write the
+//  below code multipke time so to optimise it we use the further below
+// user.name
+
+//created a const and {what value to be extracted} = from where
+// const {name} = user
+// console.log(name);
+
+
+// can provide more easy to wrte name for the objects keys
+// const {company_name:cn} = user
+// console.log(cn);
+
+
+// json API intro 
+//can use to study json formatter for studying json file
