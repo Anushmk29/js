12a4fa2b96c9cfceb75d0e8f432ef224cacfa4b9
Anush MK <101824229+Anushmk29@users.noreply.github.com>
2024-02-21 02:10:11 +0000

    done

--- .devcontainer/objects/function_with_objects.js
+++ .devcontainer/objects/function_with_objects.js
@@ -1,2 +1,54 @@
 // when we  dont know how many parameters to be add 
-function  calculateCartPrice()
+
+
+
+
+// function  calculateCartPrice(num1)
+// {
+//  return num1
+// }
+
+// console.log(calculateCartPrice(2))
+//use rest operator for more inputs 
+//...nums1 rest operator all the inputs gets or is stored in a bundle form or array
+
+// function  calculateCartPrice(val1,val2,...num1)//[ 400, 20000 ]
+// {
+//  return num1
+// }
+// console.log(calculateCartPrice(200,300,400,20000))
+//[ 200, 300, 400 ]
+// const user ={
+//     name:"anush",
+//     price:199
+//     //if the name of key changes o/p will be undefined but can solve that using typecheck or using if conditions
+// }
+// function handleObjectfunction (any1) {
+//     console.log(`username is ${any1.name} and price is `);
+// }
+// handleObjectfunction (user)
+//can add direct object
+// handleObjectfunction ({
+//     name:"san"
+// })
+
+
+//can do this fofr array also
+// const myNewArray = [200,300,400,100]
+// function arrayout(any){
+//     return any[1]
+// }
+// console.log(arrayout(myNewArray))
+
+
+// global and local scope
+if (true) {
+    let a=10
+const b =20
+var c =30
+console.log(a)//block scope
+}
+
+// console.log(a)
+// console.log(b)
+console.log(c)
